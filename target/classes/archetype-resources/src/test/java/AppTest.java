bb0fae876d4897d280db34ebee7a68380b423ceb
Andy Baxter <andrewbaxter439@gmail.com>
2023-06-14 23:48:00 +0100

    working version 1.0.0

---
+++ target/classes/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,34 @@
+package $package;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void runs() {
+        App test_app = new App();
+        Assertions.assertInstanceOf(App.class, test_app);
+    }
+}
